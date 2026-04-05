2cafc0baf241bb7c0a6945e07b1e16373e3abb00
cool-guys-bfc2 <lamarasingham@icloud.com>
2026-04-05 13:41:21 -0400

    Update run.sh

--- src/run.sh
+++ src/run.sh
@@ -1,5 +1,11 @@
-run () {
-  cd ~/bin
+setup() {
+  mkdir ~/bin/codeport
+}
+install() {
+  cdir=$(pwd)
+  cd ~/bin/codeport
   touch $1
-  
+  curl -o $1 https://raw.githubusercontent.com/cool-guys-bfc2/codport-shell/refs/heads/main/src/$1.sh
+  chmod +x $1
+  cd ${cdir}
 }
