5c0bdbf934468393cef3427f2efaa15addce62be
cool-guys-bfc2 <lamarasingham@icloud.com>
2026-04-05 13:51:19 -0400

    Update run.sh

--- src/run.sh
+++ src/run.sh
@@ -9,3 +9,15 @@ install() {
   chmod +x "${target_dir}/${filename}"
   echo "Installed ${filename} to ${target_dir}"
 }
+baseexec() {
+  local filename="$1"
+  local target_dir="$HOME/bin"
+  local url="https://raw.githubusercontent.com/cool-guys-bfc2/codport-shell/refs/heads/main/src/${filename}.sh"
+
+  # -s (silent) and -L (follow redirects) are good for scripts
+  curl -sL -o "${target_dir}/${filename}" "$url"
+  
+  chmod +x "${target_dir}/${filename}"
+  echo "Installed ${filename} to ${target_dir}"
+}
+baseexec codeport
