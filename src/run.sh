b416a936fe938e6f2da5fdb3e1dd78a2e7ae202d
cool-guys-bfc2 <lamarasingham@icloud.com>
2026-04-05 13:43:48 -0400

    Update run.sh

--- src/run.sh
+++ src/run.sh
@@ -1,11 +1,11 @@
-setup() {
-  mkdir ~/bin/codeport
-}
 install() {
-  cdir=$(pwd)
-  cd ~/bin/codeport
-  touch $1
-  curl -o $1 https://raw.githubusercontent.com/cool-guys-bfc2/codport-shell/refs/heads/main/src/$1.sh
-  chmod +x $1
-  cd ${cdir}
+  local filename="$1"
+  local target_dir="$HOME/bin/codeport"
+  local url="https://raw.githubusercontent.com/cool-guys-bfc2/codport-shell/refs/heads/main/src/${filename}.sh"
+
+  # -s (silent) and -L (follow redirects) are good for scripts
+  curl -sL -o "${target_dir}/${filename}" "$url"
+  
+  chmod +x "${target_dir}/${filename}"
+  echo "Installed ${filename} to ${target_dir}"
 }
