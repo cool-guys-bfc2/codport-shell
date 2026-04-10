52e9b4f6f5e98ebf7d16ecffb1f2db428504dd09
cool-guys-bfc2 <lamarasingham@icloud.com>
2026-04-10 10:35:01 -0400

    Update pkg.sh

--- src/pkg.sh
+++ src/pkg.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
 url=$1
+args="${@:2}"
 # Fetch the file and process it line by line
 n=0
 a='run'
+pkg='blank'
 docmd() {
   a=$1
   f=$2
-  if [ "$a"=="run" ]; then
+  if [ "$a"=="runuri" ]; then
     curl -s $f | bash
   fi
+  if [ "$a"=="pkg" ]; then
+    pkg=$f
+    mkdir -p "~/pkg/${f}"
+    cd "~/pkg/${f}"
+  fi
+  if [ "$a"=="download" ]; then
+    curl -O -s $f
+  fi
+  if [ "$a"=="exec" ]; then
+    chmod +x $f
+    source $f
+  fi
+  if [ "$a"=="execlog" ]; then
+    chmod +x $f
+    source $f > "output.log"
+  fi
+  
 }
 while IFS= read -r line || [[ -n "$line" ]]; do
     # Perform actions on each line
