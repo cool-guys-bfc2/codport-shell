9f4d7bcade3bba347493b1f5d73203df149ba159
cool-guys-bfc2 <lamarasingham@icloud.com>
2026-04-10 10:37:08 -0400

    Update fetch.sh

--- src/fetch.sh
+++ src/fetch.sh
@@ -1,6 +1,6 @@
 mkdir -p '~/pkg'
 name=$1
-url="https://raw.githubusercontent.com/cool-guys-bfc2/codport-shell/refs/heads/main/pkgs/${name}.txt"
+url="https://raw.githubusercontent.com/cool-guys-bfc2/codport-shell/refs/heads/main/pkgs/${name}/${name}.txt"
 echo "Installing package at ${url} as ${name}!"
 response=$(curl -s "${url}")
 echo "$response" > "~/pkg/${name}.txt"
