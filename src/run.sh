8be7eece1854ece1d07dea0539e62920ff46f405
cool-guys-bfc2 <lamarasingham@icloud.com>
2026-04-10 14:30:15 -0400

    Update run.sh

--- src/run.sh
+++ src/run.sh
@@ -1,32 +1,19 @@
 mkdir -p "$HOME/bin/cpd"
 cmd() {
-  local filename="$1"
-  local target_dir="$HOME/bin/cpd"
-  local url="https://cool-guys-bfc2.github.io/codport-shell/src/${filename}.sh"
+  local name="$1"
+  local filename="cp-${name}"
+  local target_dir="$HOME/bin"
+  local url="https://cool-guys-bfc2.github.io/codport-shell/src/${name}.sh"
   # -s (silent) and -L (follow redirects) are good for scripts
   curl -sL -o "${target_dir}/${filename}" "$url"
   
   chmod +x "${target_dir}/${filename}"
-  echo "Installed ${filename} to ${target_dir}"
+  echo "Installed command ${filename}"
 }
-baseexec() {
-  local filename="$1"
-  local target_dir="$HOME/bin"
-  local url="https://cool-guys-bfc2.github.io/codport-shell/src/${filename}.sh"
-  rm -f "${target_dir}/${filename}.sh"
-  # -s (silent) and -L (follow redirects) are good for scripts
-  curl -sL -o "${target_dir}/${filename}.sh" "$url"
-  
-  chmod +x "${target_dir}/${filename}.sh"
-  echo "Installed ${filename} to ${target_dir}"
-}
-baseexec cp
 cmd pkg
 cmd install
 cmd fetch
-chmod +x "$HOME/bin/cp"
 if [ ! -f "$HOME/bin/cpconfig.txt" ]; then
-    echo 'export PATH="$HOME/bin/codeport:$PATH"' >> ~/.bashrc
     echo "state=new" > "$HOME/bin/cpconfig.txt"
     source ~/.bashrc
 fi
@@ -34,4 +21,5 @@ if [ -f "$HOME/bin/cpconfig.txt" ]; then
     echo "state=update" > "$HOME/bin/cpconfig.txt"
 fi
 config=$(cat "$HOME/bin/cpconfig.txt")
+echo "Config data: "
 echo "${config}"
