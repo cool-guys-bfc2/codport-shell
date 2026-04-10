e31054e44b23332ab61fda78904b31c8c55d852b
cool-guys-bfc2 <lamarasingham@icloud.com>
2026-04-10 17:49:58 -0400

    Update update.sh

--- src/update.sh
+++ src/update.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+install() {
+  echo "Adding startup variables"
+  local filename="$1"
+  local url="https://cool-guys-bfc2.github.io/codport-shell/src/${filename}"
+  echo "Fetching script"
+  curl -sL -o "${filename}" "$url"
+  echo "Giving script execute permissions"
+  chmod +x "${filename}"
+  echo "Executing script"
+  bash ${filename}
+  echo "Job complete"
+}
+install run.sh
