bdc6a8e310d249bdc9fb1dce6e35650ca632ac00
cool-guys-bfc2 <lamarasingham@icloud.com>
2026-04-10 16:44:27 -0400

    Update pkg.sh

--- src/pkg.sh
+++ src/pkg.sh
@@ -21,7 +21,7 @@ docmd() {
   esac
 }
 
-input_file="$HOME/pkg/${url}.txt" Assuming .sh or .txt based on your logic
+input_file="$HOME/pkg/${url}.txt"
 
 while IFS= read -r line || [[ -n "$line" ]]; do
     if (( n % 2 == 0 )); then
