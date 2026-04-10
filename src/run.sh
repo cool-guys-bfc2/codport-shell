f193ce37013561e392e25b6a86b6f8b908d5af40
cool-guys-bfc2 <lamarasingham@icloud.com>
2026-04-10 15:43:19 -0400

    Update run.sh

--- src/run.sh
+++ src/run.sh
@@ -2,14 +2,15 @@ mkdir -p "$HOME/bin/cpd"
 term() {
   local name="$1"
   local fn="$2"
+  local a="$3"
   local code="${name}() {
-  if [ \$# -eq 0 ]; then
+  if [ ${a} -eq 0 ]; then
     source ${fn}
   fi
-  if [ \$# -eq 1 ]; then
+  if [ ${a} -eq 1 ]; then
     source ${fn} '\$1'
   fi
-  if [ \$# -eq 2 ]; then
+  if [ ${a} -eq 2 ]; then
     source ${fn} '\$1' '\$2'
   fi
   }"
@@ -18,18 +19,19 @@ term() {
 }
 cmd() {
   local name="$1"
+  local args="$2"
   local filename="cp-${name}"
   local target_dir="$HOME/bin"
   local url="https://cool-guys-bfc2.github.io/codport-shell/src/${name}.sh"
   # -s (silent) and -L (follow redirects) are good for scripts
   curl -sL -o "${target_dir}/${filename}" "$url"
-  term "${filename}" "${target_dir}/${filename}"
+  term "${filename}" "${target_dir}/${filename}" ${args}
   chmod +x "${target_dir}/${filename}"
   echo "Installed command ${filename}"
 }
-cmd pkg
-cmd install
-cmd fetch
+cmd pkg 1
+cmd install 1
+cmd fetch 1
 if [ -f "$HOME/bin/cpconfig.txt" ]; then
     echo "state=update" > "$HOME/bin/cpconfig.txt"
 fi
