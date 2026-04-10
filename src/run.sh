b68298a8820d67a02f294fef48d21ea9f62c22c5
cool-guys-bfc2 <lamarasingham@icloud.com>
2026-04-10 12:42:41 -0400

    Update run.sh

--- src/run.sh
+++ src/run.sh
@@ -2,7 +2,7 @@ mkdir -p "$HOME/bin/codeport"
 cmd() {
   local filename="$1"
   local target_dir="$HOME/bin/codeport"
-  local url="https://cool-guys-bfc2.github.io/codport-shell/src/${filename}.sh"
+  local url="https://cool-guys-bfc2.github.io/codport-shell/src/$1.sh"
   # -s (silent) and -L (follow redirects) are good for scripts
   curl -sL -o "${target_dir}/${filename}.sh" "$url"
   
@@ -12,7 +12,7 @@ cmd() {
 baseexec() {
   local filename="$1"
   local target_dir="$HOME/bin"
-  local url="https://cool-guys-bfc2.github.io/codport-shell/src/${filename}.sh"
+  local url="https://cool-guys-bfc2.github.io/codport-shell/src/$1.sh"
   rm -f "${target_dir}/${filename}.sh"
   # -s (silent) and -L (follow redirects) are good for scripts
   curl -sL -o "${target_dir}/${filename}" "$url"
