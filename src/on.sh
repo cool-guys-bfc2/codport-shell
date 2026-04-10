9252976ccda2c5f694e5e76ab9213a4810535ed9
cool-guys-bfc2 <lamarasingham@icloud.com>
2026-04-10 16:56:53 -0400

    Create on.sh

---
+++ src/on.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+url=$1
+args="${@:2}"
+n=0
+
+docmd() {
+  local action=$1
+  local target=$2
+  
+  case "$action" in
+    "runuri")  curl -s "$target" | bash ;;
+    "pkg")     pkg="$target"
+               mkdir -p "$HOME/pkg/$target"
+               cd "$HOME/pkg/$target" || exit ;;
+    "download") curl -O -s "$target" ;;
+    "exec")    chmod +x "$target"; source "./$target" ;;
+    "execlog") chmod +x "$target"; source "./$target" > "output.log" ;;
+    "execarg") chmod +x "$target"; source "./$target" $args ;;
+    "execute") chmod +x "$target"; source "./$target" $args > "output.log" ;;
+    "bash")    echo "$target" > "cache.sh"; bash "cache.sh" ;;
+  esac
+}
+
+input_file="$HOME/pkg/${url}.txt"
+
+while IFS= read -r line || [[ -n "$line" ]]; do
+    if (( n % 2 == 0 )); then
+      action_cmd="$line"
+    else
+      docmd "$action_cmd" "$line"
+    fi
+    n=$((n + 1))
+done < "$input_file"
