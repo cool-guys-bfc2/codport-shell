bd73a29f1422f95046735c3b885e34ee710a007e
cool-guys-bfc2 <lamarasingham@icloud.com>
2026-04-17 12:26:26 -0700

    Update make.sh

--- src/make.sh
+++ src/make.sh
@@ -2,11 +2,22 @@
 # Define variables
 FILE="$1.tar.gz"
 DEST="codapp"
-
+mkdir -p "$HOME/bin/cod-$1/"
+mkdir -p "$HOME/bin/cod-$1/exec"
 # 1. Create the new folder (if it doesn't already exist)
 mkdir -p "$DEST"
 
 # 2. Extract the file into the new folder
 # -x: Extract, -v: Verbose output, -z: Unzip (for .gz), -f: File name, -C: Change to directory
 tar -xvzf "$FILE" -C "$DEST"
+for file in *; do
+    if [[ -f "$file" ]]; then
+        if [[ "$file" == *.txt ]]; then
+            cp "$file" "$HOME/pkg
+        else
+            cp "$file" "$HOME/bin/cod-$1/exec"
+        fi
+    fi
+done
+
 rm -rf "$DEST"
