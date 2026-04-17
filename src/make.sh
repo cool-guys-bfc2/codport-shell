cecabc57089e136eaa8f44bc0d4c4d725fa840d8
cool-guys-bfc2 <lamarasingham@icloud.com>
2026-04-17 12:17:18 -0700

    Create make.sh

---
+++ src/make.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Define variables
+FILE="$1.tar.gz"
+DEST="$1"
+
+# 1. Create the new folder (if it doesn't already exist)
+mkdir -p "$DEST"
+
+# 2. Extract the file into the new folder
+# -x: Extract, -v: Verbose output, -z: Unzip (for .gz), -f: File name, -C: Change to directory
+tar -xvzf "$FILE" -C "$DEST"
