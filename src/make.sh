4aa944e34165a92debbe5d1b52ba2ee1e7bb87c3
cool-guys-bfc2 <lamarasingham@icloud.com>
2026-04-17 12:19:45 -0700

    Update make.sh

--- src/make.sh
+++ src/make.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Define variables
 FILE="$1.tar.gz"
-DEST="$1"
+DEST="codapp"
 
 # 1. Create the new folder (if it doesn't already exist)
 mkdir -p "$DEST"
@@ -9,3 +9,4 @@ mkdir -p "$DEST"
 # 2. Extract the file into the new folder
 # -x: Extract, -v: Verbose output, -z: Unzip (for .gz), -f: File name, -C: Change to directory
 tar -xvzf "$FILE" -C "$DEST"
+rm -rf "$DEST"
