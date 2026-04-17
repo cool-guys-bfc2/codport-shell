d0857cee92e7bdc0a31748ca0625dd0fe6275842
cool-guys-bfc2 <lamarasingham@icloud.com>
2026-04-17 12:09:56 -0700

    Update run.sh

--- src/run.sh
+++ src/run.sh
@@ -43,8 +43,8 @@ if [ -f "$HOME/bin/cpconfig.txt" ]; then
     echo "state=update" > "$HOME/bin/cpconfig.txt"
 fi
 if [ ! -f "$HOME/bin/cpconfig.txt" ]; then
-    echo 'source "\$HOME/cpf.sh"' >> "$HOME/.bashrc"
-    echo 'source "\$HOME/cpf.sh"' >> "$HOME/.zshrc"
+    echo 'source cpf.sh' >> "$HOME/.bashrc"
+    echo 'source cpf.sh' >> "$HOME/.zshrc"
     echo "state=new" > "$HOME/bin/cpconfig.txt"
 fi
 config=$(cat "$HOME/bin/cpconfig.txt")
